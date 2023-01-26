1b051c29e772b397b0adeb283daf9242d483e1a3
tntran <tntran@unc.edu>
2023-01-25 19:55:45 -0500

    add error to long and lat

--- cli.js
+++ cli.js
@@ -26,8 +26,8 @@ if (argv["n"] && argv['s']) {
     console.log("Cannot specify LATITUDE twice");
     process.exit(1);
 }
-latitude = argv["n"] || -argv["s"] || 0;
-if (Math.abs(latitude) > 90) {
+latitude = argv["n"] || -argv["s"];
+if (!(latitude <= 90 && latitude >= -90)) {
     console.log("Latitude must be in range");
     process.exit(1);
 }
@@ -36,8 +36,8 @@ if (argv["e"] && argv['w']) {
     console.log("Cannot specify LONGITUDE twice");
     process.exit(1);
 }
-longitude = argv["e"] || -argv["w"] || 0;
-if (Math.abs(longitude) > 180) {
+longitude = argv["e"] || -argv["w"];
+if (!(longitude <= 180 && longitude >= -180)) {
     console.log("Longitude must be in range");
     process.exit(1);
 }
